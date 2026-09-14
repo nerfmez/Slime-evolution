392da14fe7938fbf022758692f1eb5d854b544e3
nerfmez <ballboy_lnw@hotmail.com>
2026-09-14 17:57:23 +0700

    Fix atlas UV orientation without recoloring the approved art

--- critters/renderer.js
+++ critters/renderer.js
@@ -17,6 +17,7 @@ void main(){
   gl_Position.xy+=q*actor.w*vec2(length(vec3(vp[0][0],vp[1][0],vp[2][0])),length(vec3(vp[0][1],vp[1][1],vp[2][1])));
   vec2 t=corner*.5+.5;
   if(misc.y>.5)t.x=1.-t.x;
+  t.y=1.-t.y;
   uv=mix(rect.xy,rect.zw,t);
   localUv=corner;special=misc.z;
 }`;
@@ -64,7 +65,7 @@ export function createCritterRenderer(gl){
   gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
   let ready=false,disposed=false;
   const image=new Image();
-  image.onload=()=>{if(disposed)return;gl.bindTexture(gl.TEXTURE_2D,texture);gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL,true);gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,image);gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL,false);ready=true;};
+  image.onload=()=>{if(disposed)return;gl.bindTexture(gl.TEXTURE_2D,texture);gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,image);ready=true;};
   image.src=ATLAS_URL;
   let capacity=0,data=new Float32Array(0);
   return {draw(souls,vp,time,player,visible=()=>true,pickups=[]){
