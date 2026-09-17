35a8c224a214f1d374c848ae1c674ec908369fce
nerfmez <ballboy_lnw@hotmail.com>
2026-09-17 09:16:06 +0700

    Create standalone new-animals-only test build

--- vfx/beads.js
+++
@@ -1,2 +0,0 @@
-// Compatibility entry for the older editable baseline.
-export {createCritterRenderer as createBeadRenderer,critterVertex as beadVertex,critterFragment as beadFragment} from '../critters/renderer.js';
--- vite.config.js
+++
@@ -1,1 +0,0 @@
-export default {base:'./',server:{host:'0.0.0.0',allowedHosts:['terminal.local']},build:{target:'es2020',rollupOptions:{input:{main:'index.html',motion:'slime-motion.html'}}}};
